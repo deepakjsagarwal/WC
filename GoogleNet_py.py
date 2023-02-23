479ff7b66a2ddc392296da5f19f223faf5e62edf
Deepak Agarwal <deepakjsagarwal@gmail.com>
2023-02-23 21:25:12 +0530

    changes GoogleNet for ParamGanga

---
+++ GoogleNet_py.py
@@ -0,0 +1,523 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# !pip install opencv-python scikit-learn scikit-image matplotlib spectral keras_tuner vis
+# !pip install tensorflow numpy pandas
+# !pip install spectral
+
+
+# In[ ]:
+
+
+import tensorflow as tf
+
+
+# In[ ]:
+
+
+from tensorflow import keras as keras
+from keras import layers as layers
+
+
+# In[ ]:
+
+
+import os, timeit
+from skimage.filters import threshold_otsu
+import numpy as np
+from math import inf as inf
+
+
+# In[ ]:
+
+
+import pandas as pd
+
+
+# In[ ]:
+
+
+from spectral.io import envi as envi
+from spectral import imshow
+
+
+# In[ ]:
+
+
+from sklearn.decomposition import IncrementalPCA
+
+
+# In[ ]:
+
+
+import sys
+
+
+# In[ ]:
+
+
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+
+# In[ ]:
+
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+print(gpus)
+
+
+# In[ ]:
+
+
+from sys import platform
+DATA_DIRECTORY = ""
+SLASH = ""
+if platform == "linux" or platform == "linux2":
+    DATA_DIRECTORY = "/home/ishu_g.iitr/wheat/data/BULK/"
+    SLASH = "/"
+elif platform == "win32":
+    DATA_DIRECTORY = "D:\mvl\wheat\data\BULK\\"
+    SLASH="\\"
+
+
+# In[ ]:
+
+
+#Constants
+BAND_NUMBER = 60
+FILLED_AREA_RATIO = 0.9
+TOTAL_IMAGE_COUNT = 2400
+IMAGE_COUNT = int(TOTAL_IMAGE_COUNT/4)
+NUM_VARIETIES = 8
+
+IMAGE_WIDTH = 30
+IMAGE_HEIGHT = 30
+
+
+# In[ ]:
+
+
+ACTIVATION_TYPE =  "relu"
+BATCH_SIZE = 2*NUM_VARIETIES
+EPOCHS = 100
+LEARNING_RATE_BASE = 0.0001
+
+
+# In[ ]:
+
+
+from enum import Enum
+
+class filter_method(Enum):
+    none = 0
+    snv = 1
+    msc = 2
+    savgol = 3
+
+FILT = 1
+FILTER = filter_method(FILT).name
+
+# to be set if filter chosen is savgol
+WINDOW = 7
+ORDER = 2
+DERIVATIVE = "none"
+
+
+# In[ ]:
+
+
+from enum import Enum
+ 
+class feature_extraction_method(Enum):
+    none = 0
+    pca_loading = 1
+    lda = 2
+    ipca = 3
+
+FEAT_EXT = 0
+FEATURE_EXTRACTION = feature_extraction_method(FEAT_EXT).name
+
+NUM_OF_BANDS = 3
+if FEATURE_EXTRACTION == "pca_loading" or FEATURE_EXTRACTION == "ipca":
+    NUM_OF_BANDS = 8
+elif FEATURE_EXTRACTION == "lda":
+    NUM_OF_BANDS = 3
+    assert NUM_OF_BANDS <= min(NUM_VARIETIES-1,168),"NUM_OF_BANDS is greater."
+
+
+REMOVE_NOISY_BANDS = False
+FIRST_BAND = 15
+LAST_BAND = 161
+
+
+# In[ ]:
+
+
+def start_timer():
+    print("Testing started")
+    return timeit.default_timer()
+
+def end_timer():
+    return timeit.default_timer()
+
+def show_time(tic,toc): 
+    test_time = toc - tic
+    print('Testing time (s) = ' + str(test_time) + '\n')
+
+
+# In[ ]:
+
+
+# List for All varieties
+VARIETIES = []
+VARIETIES_CODE = {}
+
+for name in os.listdir(DATA_DIRECTORY):
+    if (name.endswith(".hdr") or name.endswith(".bil")):
+        continue
+    VARIETIES_CODE[name] = len(VARIETIES)
+    VARIETIES.append(name)
+    if len(VARIETIES)==NUM_VARIETIES:
+        break
+
+
+# In[ ]:
+
+
+def dataset_file_name(variety):
+    name = "./dataset/"+str(variety).zfill(3)+"_IC_"+str(TOTAL_IMAGE_COUNT).zfill(5)+"_FilledArea_"+str(FILLED_AREA_RATIO)+"_BandNo_"+str(BAND_NUMBER)+"_ImageHeight_"+str(IMAGE_HEIGHT)+"_ImageWidth_"+str(IMAGE_WIDTH)
+    if FILT != 0:
+        name+="_FILTER_"+str(FILTER)
+    if FEAT_EXT !=0:
+        name+="_FeatureExtraction_"+str(FEATURE_EXTRACTION)+"_NumOfBands_"+str(NUM_OF_BANDS)
+    if REMOVE_NOISY_BANDS:
+        name+="_REMOVE_NOISY_BANDS_"+str(REMOVE_NOISY_BANDS)+"_FB_"+str(FIRST_BAND)+"_LB_"+str(LAST_BAND)
+    if FILTER == "savgol":
+        name+="_WINDOW_"+str(WINDOW)+"_ORDER_"+str(ORDER)
+    return name
+
+
+# In[ ]:
+
+
+print(VARIETIES[:NUM_VARIETIES])
+
+
+# In[ ]:
+
+
+train_dataset = []
+train_dataset_label = []
+test_dataset=[]
+test_dataset_label = []
+
+for idx, v in enumerate(VARIETIES):
+    print("idx: ",idx)
+    if idx >= NUM_VARIETIES:
+        break
+    train_dataset= train_dataset + np.load(dataset_file_name(v)+"_train_dataset.npy").tolist()
+    train_dataset_label = train_dataset_label + np.load(dataset_file_name(v)+"_train_dataset_label.npy").tolist()
+    test_dataset = test_dataset + np.load(dataset_file_name(v)+"_test_dataset.npy").tolist()
+    test_dataset_label = test_dataset_label + np.load(dataset_file_name(v)+"_test_dataset_label.npy").tolist()
+    
+train_dataset = np.array(train_dataset)
+train_dataset_label = np.array(train_dataset_label)
+test_dataset = np.array(test_dataset)
+test_dataset_label = np.array(test_dataset_label)
+
+
+# In[ ]:
+
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow.keras import datasets, layers, models, losses, Model
+from keras.layers import Input, Conv2D, MaxPool2D, MaxPooling2D, Activation, Flatten, Dense, AveragePooling2D, GlobalAveragePooling2D
+from keras.layers.core import Dropout
+from keras.optimizers import Adam
+
+
+# In[ ]:
+
+
+def normalizeDataWholeSeed(data,normalization_type='max'):
+    
+    if normalization_type == 'max':
+        for idx in range(data.shape[0]):
+            data[idx,:,:,:] = data[idx,:,:,:]/np.max(abs(data[idx,:,:,:]))
+            
+    elif normalization_type == 'l2norm':
+        from numpy import linalg as LA
+        for idx in range(data.shape[0]):
+            data[idx,:,:,:] = data[idx,:,:,:]/LA.norm(data[idx,:,:,:])       
+        
+    return data
+
+
+# In[ ]:
+
+
+def inception(x,filters_1x1,filters_3x3_reduce,filters_3x3,filters_5x5_reduce,filters_5x5,filters_pool,activation_type='relu'):
+    path1 = Conv2D(filters_1x1,        (1, 1), padding='same', activation=activation_type)(x)
+    
+    path2 = Conv2D(filters_3x3_reduce, (1, 1), padding='same', activation=activation_type)(x)
+    path2 = Conv2D(filters_3x3,        (1, 1), padding='same', activation=activation_type)(path2)
+    
+    path3 = Conv2D(filters_5x5_reduce, (1, 1), padding='same', activation=activation_type)(x)
+    path3 = Conv2D(filters_5x5,        (1, 1), padding='same', activation=activation_type)(path3)
+    
+    path4 = MaxPool2D((3, 3),  strides=(1, 1), padding='same')(x)
+    path4 = Conv2D(filters_pool,       (1, 1), padding='same', activation=activation_type)(path4)
+    
+    return tf.concat([path1, path2, path3, path4], axis=3)
+
+
+# In[ ]:
+
+
+def auxiliary_classifier(x,num_classes,activation_type='relu'):
+    aux = AveragePooling2D((5, 5), strides=3)(x)
+    aux = Conv2D(128, 1, padding='same', activation=activation_type)(aux)
+    aux = Flatten()(aux)
+    aux = Dense(1024, activation=activation_type)(aux)
+    aux = Dropout(0.7)(aux)
+    aux = Dense(num_classes, activation='softmax')(aux)
+    return aux
+
+
+# In[ ]:
+
+
+def GoogleNetModel(data_num_rows, data_num_cols, num_input_chans=1, num_classes=NUM_VARIETIES, activation_type='relu', dropout_rate=0.0):
+
+    inp = Input(shape=(data_num_rows, data_num_cols, num_input_chans))
+    input_tensor = layers.experimental.preprocessing.Resizing(224, 224, interpolation="bilinear", input_shape=train_dataset.shape[1:])(inp)
+    x = Conv2D(64,  7, strides=2, padding='same', activation=activation_type)(input_tensor)
+    x = MaxPooling2D(3, strides=2)(x)
+    x = Conv2D(64,  1, strides=1, padding='same', activation=activation_type)(x)
+    x = Conv2D(192, 3, strides=1, padding='same', activation=activation_type)(x)
+    x = MaxPooling2D(3, strides=2)(x)
+    x = inception(x, filters_1x1=64 , filters_3x3_reduce=96 , filters_3x3=128, filters_5x5_reduce=16, filters_5x5=32, filters_pool=32)
+    x = inception(x, filters_1x1=128, filters_3x3_reduce=128, filters_3x3=192, filters_5x5_reduce=32, filters_5x5=96, filters_pool=64)
+    x = MaxPooling2D(3, strides=2)(x)
+    x = inception(x, filters_1x1=192, filters_3x3_reduce=96 , filters_3x3=208, filters_5x5_reduce=16, filters_5x5=48, filters_pool=64)
+    
+    aux1 = auxiliary_classifier(x,num_classes)
+    
+    x = inception(x, filters_1x1=160, filters_3x3_reduce=112, filters_3x3=224, filters_5x5_reduce=24, filters_5x5=64, filters_pool=64)
+    x = inception(x, filters_1x1=128, filters_3x3_reduce=128, filters_3x3=256, filters_5x5_reduce=24, filters_5x5=64, filters_pool=64)
+    x = inception(x, filters_1x1=112, filters_3x3_reduce=144, filters_3x3=288, filters_5x5_reduce=32, filters_5x5=64, filters_pool=64)
+    
+    aux2 = auxiliary_classifier(x,num_classes)
+    
+    x = inception(x, filters_1x1=256, filters_3x3_reduce=160, filters_3x3=320, filters_5x5_reduce=32, filters_5x5=128, filters_pool=128)
+    x = MaxPooling2D(3, strides=2)(x)
+    x = inception(x, filters_1x1=256, filters_3x3_reduce=160, filters_3x3=320, filters_5x5_reduce=32, filters_5x5=128, filters_pool=128)
+    x = inception(x, filters_1x1=384, filters_3x3_reduce=192, filters_3x3=384, filters_5x5_reduce=48, filters_5x5=128, filters_pool=128)
+    x = GlobalAveragePooling2D()(x)
+    x = Dropout(dropout_rate)(x)
+    out = Dense(num_classes, activation='softmax')(x)
+    
+    return Model(inputs = inp, outputs = [out, aux1, aux2])
+
+
+# In[ ]:
+
+
+def getGoogleNetModel():
+    learning_rate_base = LEARNING_RATE_BASE
+    activation_type = ACTIVATION_TYPE
+    wheat_types =  VARIETIES
+    num_classes = len(wheat_types)
+    dropout_rate = 0.4
+    print("--------------Load Data--------------")
+
+    x_training = np.array(train_dataset)
+    labels_training = np.array(train_dataset_label)
+    
+    # Normalize the data
+    x_training = normalizeDataWholeSeed(x_training)
+    
+    # Extract some information
+    num_train = x_training.shape[0]
+    N_spatial = x_training.shape[1:3]
+    N_channel = x_training.shape[3]
+    
+    print("--------------Done--------------")
+    
+    ############ Create a model ############
+    print("--------------Create a model--------------")
+    
+    # Generate a model
+    model = GoogleNetModel(data_num_rows = N_spatial[0], 
+                           data_num_cols = N_spatial[1],
+                           num_input_chans = N_channel, 
+                           num_classes = num_classes,
+                           activation_type = activation_type,
+                           dropout_rate = dropout_rate)
+
+    # Compile the model
+    adam_opt = Adam(learning_rate=LEARNING_RATE_BASE, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.01)
+    model.compile(optimizer=adam_opt, loss=[losses.sparse_categorical_crossentropy,losses.sparse_categorical_crossentropy,losses.sparse_categorical_crossentropy],loss_weights=[1, 0.3, 0.3],metrics=['accuracy'])
+    print("---------Completed---------")
+    return model
+
+
+# In[ ]:
+
+
+def plot_graph(df,title,xlabel,ylabel,values=['loss'],legends=[]):
+    
+    for value in values:
+        epoch_count = range(1, len(df.index) + 1)
+        plt.plot(epoch_count, df[value].tolist())
+    plt.title(title)
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
+    if legends==[]:
+        legends = values
+    plt.legend(legends, loc='upper left')
+    plt.show()
+
+
+# In[ ]:
+
+
+model = getGoogleNetModel()
+
+
+# In[ ]:
+
+
+print(model.summary())
+
+
+# In[ ]:
+
+
+x_train = []
+y_train = []
+x_val = []
+y_val = []
+
+for i in range(len(train_dataset)):
+    if i%5==0:
+        x_val.append(train_dataset[i])
+        y_val.append(train_dataset_label[i])
+    else:
+        x_train.append(train_dataset[i])
+        y_train.append(train_dataset_label[i])
+        
+x_train = np.array(x_train)
+y_train = np.array(y_train)
+y_train = [y_train,y_train,y_train]
+
+x_val = np.array(x_val)
+y_val = np.array(y_val)
+y_val = [y_val,y_val,y_val]
+
+
+# In[ ]:
+
+
+train_eva = []
+test_eva = []
+
+
+# In[ ]:
+
+
+history_dataframe = pd.DataFrame() 
+
+
+# In[ ]:
+
+
+tic = start_timer()
+for x in range(EPOCHS):
+    print("\nEpoch: ",x+1)
+    model.fit(x_train ,y_train ,batch_size=BATCH_SIZE ,epochs=1, verbose=2, validation_data=(x_val, y_val), shuffle=True)
+    if history_dataframe.size == 0:
+        history_dataframe = pd.DataFrame.from_dict(history.history)
+    else:
+        history_dataframe = pd.concat([history_dataframe,pd.DataFrame.from_dict(history.history)],axis=0,ignore_index=True)
+    print("for testing")
+    print(model.evaluate(test_dataset,test_dataset_label))
+    print("for training")
+    print(model.evaluate(train_dataset,train_dataset_label))
+toc = end_timer()
+show_time(tic,toc)
+
+
+# In[ ]:
+
+
+print(history_dataframe)
+
+
+# In[ ]:
+
+
+train_eva_df = pd.DataFrame(train_eva,columns=['loss','dense_4_loss','dense_1_loss','dense_3_loss','dense_4_accuracy','dense_1_accuracy','dense_3_accuracy'])
+test_eva_df = pd.DataFrame(test_eva,columns=['loss','dense_4_loss','dense_1_loss','dense_3_loss','dense_4_accuracy','dense_1_accuracy','dense_3_accuracy'])
+
+
+# In[ ]:
+
+
+plot_graph(train_eva_df,'Model Train Loss','Epoch','Loss',['loss'],['train_loss'])
+plot_graph(test_eva_df,'Model Test Loss','Epoch','Loss',['loss'],['test_loss'])
+
+
+# In[ ]:
+
+
+plot_graph(train_eva_df,'Model Train Accuracy','Epoch','Dense Accuracy',['dense_4_accuracy', 'dense_1_accuracy', 'dense_3_accuracy'])
+plot_graph(test_eva_df,'Model Test Accuracy','Epoch','Dense Accuracy',['dense_4_accuracy', 'dense_1_accuracy', 'dense_3_accuracy'])
+
+
+# In[ ]:
+
+
+plot_graph(train_eva_df,'Model Train Loss','Epoch','Dense Loss',['dense_4_loss', 'dense_1_loss', 'dense_3_loss'])
+plot_graph(test_eva_df,'Model Test Loss','Epoch','Dense Loss',['dense_4_loss', 'dense_1_loss', 'dense_3_loss'])
+
+
+# In[ ]:
+
+
+plot_graph(history_dataframe,'Model Loss','Epoch','Loss',['loss','val_loss'])
+
+
+# In[ ]:
+
+
+plot_graph(history_dataframe,'Model Train Accuracy','Epoch','Dense Accuracy',['dense_4_accuracy', 'dense_1_accuracy', 'dense_3_accuracy'])
+
+
+# In[ ]:
+
+
+plot_graph(history_dataframe,'Model Train Loss','Epoch','Dense Loss',['dense_4_loss', 'dense_1_loss', 'dense_3_loss'])
+
+
+# In[ ]:
+
+
+plot_graph(history_dataframe,'Model Val Accuracy','Epoch','Validation Accuracy',['val_dense_4_accuracy', 'val_dense_1_accuracy', 'val_dense_3_accuracy'])
+
+
+# In[ ]:
+
+
+plot_graph(history_dataframe,'Model Val Loss','Epoch','Validation Loss',['val_dense_4_loss', 'val_dense_1_loss', 'val_dense_3_loss'])
+
+
+# In[ ]:
+
+
+
+
