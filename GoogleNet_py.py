2ad52fdda088161e73ff564947f878fb466ab4ab
Ishu Gupta <ishugupta0298@gmail.com>
2023-02-24 10:18:46 +0530

    comment out gpu lines form googlenet

--- GoogleNet_py.py
+++ GoogleNet_py.py
@@ -59,14 +59,14 @@ import sys
 # In[9]:
 
 
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+# print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
 
 # In[10]:
 
 
-gpus = tf.config.experimental.list_physical_devices('GPU')
-print(gpus)
+# gpus = tf.config.experimental.list_physical_devices('GPU')
+# print(gpus)
 
 
 # In[11]:
